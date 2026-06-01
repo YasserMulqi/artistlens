de6368914466e9b23d43cd452a63454acfb6e490
YasserMulqi <ymulqi@gmail.com>
2026-06-02 01:09:47 +0300

    Add app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import HeroSection from '@/components/HeroSection'
+import IntroSection from '@/components/IntroSection'
+import ServicesSection from '@/components/ServicesSection'
+import HorizontalScroll from '@/components/HorizontalScroll'
+import VideoSection from 7@/components/VideoSection'
+import ProcessSection from '@/components/ProcessSection'
+import CTASection from '@/components/CTASection'
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <IntroSection />
+      <ServicesSection />
+      <HorizontalScroll />
+      <VideoSection />
+      <ProcessSection />
+      <CTASection />
+    </>
+  )
+}
